62bb77c9013aaffded10027bfd7e6af406a42b10
asustuf <juanantoniodearos@gmail.com>
2024-05-17 16:02:53 +0200

    Paso 1 de vista controlador

--- ProyectoPolideportivo/src/main/java/util/FileManager.java
+++ ProyectoPolideportivo/src/main/java/util/FileManager.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 public class FileManager<T> {
 
+    public static final String LISTA_SANCIONES = "listasan";
 
     public static final String LISTA_USUARIOS = "listausu";
     public static final String NUMERO_USUARIO = "numUser";
